14676e0910ac472f1a5e1937390e508d3d6f3857
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-04 14:50:45 +0700

    test state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+    state = {
+        date : new Date()
+    }
     render() {
         return (
             <div className="App">
                 <h1>Product Management System</h1>
+                <p>{this.state.date.toLocaleTimeString()}</p>
             </div>
         )
     }
