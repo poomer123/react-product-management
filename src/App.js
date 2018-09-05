550e4e6f51e2cd11bb9714663c0ca443de40f5ed
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-05 10:21:21 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
-import Footer from "./Footer";
+import Header from "./Header"
+import Footer from "./Footer"
 
 class App extends Component {
-    state = {
-        date : new Date()
-    }
     render() {
         return (
             <div className="container-fluid">
+                <Header />
                 <h1>Product Management System</h1>
-                <p>{this.state.date.toLocaleTimeString()}</p>
                 <Footer company="Kickdown" email="info@kickdown.in.th" />
             </div>
         )
---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react"
+
+class Header extends Component {
+    state = {
+        date : new Date()
+    }
+    componentDidMount() {
+       this.timerId = setInterval( () => this.setTimeToState(), 1000 )
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerId)
+    }
+    setTimeToState() {
+        this.setState({
+            date: new Date()
+        })
+    }
+    render() {
+        return (
+            <div className="row mt-3">
+                <div className="col-12 col-md-8">
+                    <h1 className="text-info"><img style={ {height: '50px'} } src="/images/logo.png" alt="Logo"/> ออเดอร์แจ่มแมว</h1>
+                </div>
+                <div className="col-12 col-md-4 text-right">
+                    <h5 className="text-muted pt-3">{this.state.date.toLocaleTimeString()}</h5>
+                </div>
+            </div> 
+        )
+    }
+}
+
+export default Header
