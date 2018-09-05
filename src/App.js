a84e4fd8df62d1cebc2246d596ad9ef380c284b7
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-05 16:53:05 +0700

    get data via GET method from local server with fetch method

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ class App extends Component {
         products : ""
     }
     componentDidMount() {
-        this.setState({
-            products : [
-                { productId: 1, productName: "สลัดผัก", unitPrice: "120", thumbnail: "/images/products/1.jpg" },
-                { productId: 2, productName: "ไก่ทอด", unitPrice: "90", thumbnail: "/images/products/2.jpg" },
-                { productId: 3, productName: "บิงซู", unitPrice: "200", thumbnail: "/images/products/3.jpg" },
-                { productId: 4, productName: "เฟรนฟราย", unitPrice: "140", thumbnail: "/images/products/4.jpg" },
-                { productId: 5, productName: "เค้ก 3 ชั้น", unitPrice: "200", thumbnail: "/images/products/5.jpg" },
-                { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", unitPrice: "140", thumbnail: "/images/products/6.jpg" }
-            ]
+        // this.setState({
+        //     products : [
+        //         { productId: 1, productName: "สลัดผัก", unitPrice: "120", thumbnail: "/images/products/1.jpg" },
+        //         { productId: 2, productName: "ไก่ทอด", unitPrice: "90", thumbnail: "/images/products/2.jpg" },
+        //         { productId: 3, productName: "บิงซู", unitPrice: "200", thumbnail: "/images/products/3.jpg" },
+        //         { productId: 4, productName: "เฟรนฟราย", unitPrice: "140", thumbnail: "/images/products/4.jpg" },
+        //         { productId: 5, productName: "เค้ก 3 ชั้น", unitPrice: "200", thumbnail: "/images/products/5.jpg" },
+        //         { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", unitPrice: "140", thumbnail: "/images/products/6.jpg" }
+        //     ]
+        // })
+        fetch('http://localhost:3001/products', {method: 'GET'})
+        .then(res => res.json())
+        .then(res => {
+            this.setState({
+                products: res
+            })
         })
     }
     render() {
