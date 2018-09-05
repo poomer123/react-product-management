4afe85e2ad8350173cc84f3e371ee05a6ad5558a
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-05 10:03:55 +0700

    edit footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,14 +3,15 @@ import React from "react"
 const Footer = (props) => {
     const {company, email} = props
     return (
-        <div className="container-fluid">
-        <hr />
-            <div className="text-center title text-uppercase">
-                <small>
-                    <span className="text-danger">Powered By {company}</span> | <span className="text-muted">Contact Us : {email}</span>
-                </small>
+        <div className="row">
+            <div className="col-12">
+                <hr />
+                <div className="text-center title text-uppercase">
+                    <small>
+                        <span className="text-danger">Powered By {company}</span> | <span className="text-muted">Contact Us : {email}</span>
+                    </small>
+                </div>
             </div>
-
         </div>
     )
 }
