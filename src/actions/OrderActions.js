f0e818f20c1da064fa7271b922b9c74d8287b70e
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-10 16:10:52 +0700

    React + Redux : Actions #2

--- src/actions/OrderActions.js
+++ src/actions/OrderActions.js
@@ -4,7 +4,7 @@ import { ORDERS_FETCH } from './types'
 export const ordersFetch = () => {
 
     return dispatch => {
-        axios.get('http://localhost:3001/orders').then(
+        axios.get('http://localhost:3001/orders/').then(
             res => {
                 dispatch({
                     type: ORDERS_FETCH,
@@ -17,5 +17,18 @@ export const ordersFetch = () => {
 }
 
 export const orderDelete = id => {
-
+    return dispatch => {
+        axios.delete('http://localhost:3001/orders/'+ id).then(
+            res => {
+                axios.get('http://localhost:3001/orders/').then(
+                    res => {
+                        dispatch({
+                            type: ORDERS_FETCH,
+                            payload: res.data
+                        })
+                    }
+                )
+            }
+        )
+    }
 }
