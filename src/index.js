1a63c8698b36d39953808944708b7e87aad9af81
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-10 14:40:15 +0700

    install dependencies and React + Redux : Store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 
+const store = createStore(reducers, {})
 
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     document.getElementById('root')
 )
 
