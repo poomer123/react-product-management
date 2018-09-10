25e37e82b65624725c2094b516f1d2fdf7fef2a5
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-10 15:46:56 +0700

    React + Redux : Actions #1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
 import reducers from './reducers/index'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
 
-const store = createStore(reducers, {})
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
     <Provider store={store}>
