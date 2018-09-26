2ef17ab7058ba515ccc062dd01189c2b382ef550
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-09-26 16:57:37 +0700

    การแสดง Errors ให้กับ Users

--- src/components/common/FormField.js
+++ src/components/common/FormField.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default ({ input, label, type, required, meta}) => {
-    console.log(meta)
     return (
         <div className='form-group'>
             <label className='title' htmlFor={input.name}>{label}</label>
             <input type={type} required={required} {...input} className='form-control' />
+
+            {
+                meta.error && meta.touched && (
+                    <div className='mt-2 text-danger title'>{meta.error}</div>
+                )
+            }
+            
         </div>
     )
 }
